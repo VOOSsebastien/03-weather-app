a48d7d39265e037b26e8eabd7c2a67cc5696e494
Sebastien Voos <sebastien.voos@hotmail.com>
2022-06-16 01:05:38 +0200

    tests

--- ex/script.js
+++ ex/script.js
@@ -5,13 +5,21 @@ const meteo = async () => {
     var lon = "5.571109";
     var part = "daily";
     const API_KEY = "e66ebcda72e60ebba6117c1d369b5693";
-    const response = await fetch('https://api.openweathermap.org/data/3.0/onecall?lat='+lat+'&lon='+lon+'&exclude='+part+'&appid='+API_KEY);
+    const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=a8fb5a447e41ecaabda52736d06000ab');
     const json = await response.json().catch(console.error);
     const list = document.createElement("ul");
     document.querySelector('.meteo').appendChild(list);
 
         var item = document.createElement('li');
-        item.appendChild(document.createTextNode(json.zip));
         list.appendChild(item);
+        let test=[];
+        
+        for(i=0;i<14;i++){
+            test=[json.coord,json.weather,json.base,json.main,json.visibility,json.wind,json.clouds,json.rain,json.sys[i],json.timezone,json.id,json.name
+        ];
+        item.innerHTML+="NEW                                        "+test[i];
+        }
+        console.log(test);
+        console.log(list);
 }
 btn.addEventListener('click', meteo);
---
+++ test/script.js
@@ -0,0 +1,36 @@
+const btn = document.getElementById("btn");
+
+function drawWeather( d ) {
+	var celcius = Math.round(parseFloat(d.main.temp)-273.15);
+	var fahrenheit = Math.round(((parseFloat(d.main.temp)-273.15)*1.8)+32); 
+	var description = d.weather[0].description;
+	
+	document.getElementById('description').innerHTML = description;
+	document.getElementById('temp').innerHTML = celcius + '&deg;';
+	document.getElementById('location').innerHTML = d.name;
+	
+	if( description.indexOf('rain') > 0 ) {
+  	document.body.className = 'sunny';
+  } else if( description.indexOf('cloud') > 0 ) {
+  	document.body.className = 'sunny';
+  } else if( description.indexOf('sunny') > 0 ) {
+  	document.body.className = 'sunny';
+  }
+}
+
+function fetching( cityID ) {
+
+	fetch('https://api.openweathermap.org/data/2.5/weather?id=' + cityID+ '&APPID=a8fb5a447e41ecaabda52736d06000ab')  
+	.then(function(resp) { return resp.json() }) // Convert data to json
+	.then(function(data) {
+		drawWeather(data); // Call drawWeather
+	})
+	.catch(function() {
+		// catch any errors
+	});
+}
+window.onload = function() {
+    fetching( 2792413 );
+  }
+  //btn.addEventListener('click',  weatherBalloon( 6167865 ));
+  console.log(d);
